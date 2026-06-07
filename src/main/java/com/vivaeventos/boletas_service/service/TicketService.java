7125b68ddfd5d987bbad4886568a9aec25a79577
JuanjoseR18 <juan.restrepo.avalo@correounivalle.edu.co>
2026-06-07 17:44:12 -0500

    Microservicio conectado a base de datos de prueba con funciones principales creado

---
+++ src/main/java/com/vivaeventos/boletas_service/service/TicketService.java
@@ -0,0 +1,41 @@
+package com.vivaeventos.boletas_service.service;
+
+import com.vivaeventos.boletas_service.dto.CreateTicketRequest;
+import com.vivaeventos.boletas_service.model.Ticket;
+import com.vivaeventos.boletas_service.model.TicketStatus;
+import com.vivaeventos.boletas_service.repository.TicketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TicketService {
+
+    private final TicketRepository repository;
+
+    public Ticket createTicket(CreateTicketRequest request) {
+
+        Ticket ticket = Ticket.builder()
+                .id(UUID.randomUUID())
+                .qrCode(UUID.randomUUID().toString())
+                .eventId(request.eventId())
+                .orderId(request.orderId())
+                .attendeeId(request.attendeeId())
+                .status(TicketStatus.ACTIVE)
+                .build();
+
+        return repository.save(ticket);
+    }
+
+    public Ticket getTicket(UUID id) {
+        return repository.findById(id)
+                .orElseThrow();
+    }
+
+    public List<Ticket> getAllTickets() {
+        return repository.findAll();
+    }
+}
